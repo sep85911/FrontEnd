a40859d22e99050e75f02fdc841b23106a82f460
Micah <sep85911@outlook.com>
2021-04-06 22:17:49 +0800

    202104062217

---
+++ js/FreeStyle.js
@@ -0,0 +1,35 @@
+console.log("fuck you!")
+
+var hButton = document.getElementById("btn001")
+
+console.log("这个hButton是个:" + typeof hButton)
+
+function clickbtn001()
+{
+    console.log(hButton.innerHTML + "   " + Date());
+    // hButton.innerHTML="点就点！"
+
+    // 改变css属性
+    hButton.style.margin = "100px"
+    hButton.style.padding = "25px"
+    hButton.style.backgroundColor = "green"
+
+    console.log(typeof hButton.onclick)
+    hButton.onclick = clickbtn001_2
+
+    console.log(typeof hButton.onclick)
+
+    hButton.onmouseover = mouseoverbtn001
+
+    console.log(typeof hButton.onclick)
+}
+
+function clickbtn001_2()
+{
+    console.log("中途改变了函数噢!")
+}
+
+function mouseoverbtn001()
+{
+    console.log('鼠标掠过!')
+}
