9a4fbf427c134a264cee190cd98348c26413bb2a
Micah <sep85911@outlook.com>
2021-04-18 21:52:27 +0800

    202104182152

--- js/FreeStyle.js
+++ js/FreeStyle.js
@@ -1,32 +1,16 @@
 console.log("fuck you!")
 
-var hButton = document.getElementById("btn001")
 
-console.log("这个hButton是个:" + typeof hButton)
+var btns = document.getElementsByClassName("btnStyle")
 
-function clickbtn001() {
-    console.log(hButton.innerHTML + "   " + Date());
-    // hButton.innerHTML="点就点！"
+for (var i = 0; i < btns.length; i++) {
+    btns[i].onclick = function (e) {
 
-    // 改变css属性
-    hButton.style.margin = "100px"
-    hButton.style.padding = "25px"
-    hButton.style.backgroundColor = "green"
+        for (var j = 0; j < btns.length; j++) {
+            btns[j].className = "btnStyle"
+        }
 
-    console.log(typeof hButton.onclick)
-    hButton.onclick = clickbtn001_2
+        this.className += " active"
 
-    console.log(typeof hButton.onclick)
-
-    hButton.onmouseover = mouseoverbtn001
-
-    console.log(typeof hButton.onclick)
-}
-
-function clickbtn001_2() {
-    console.log("中途改变了函数噢!")
-}
-
-function mouseoverbtn001() {
-    console.log('鼠标掠过!')
+    }
 }
