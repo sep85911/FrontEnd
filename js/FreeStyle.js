e8bd1ef53b2d54c0cc12b7f91a21058b5699bfa7
Micah <sep85911@outlook.com>
2021-04-19 23:57:56 +0800

    202104192357

--- js/FreeStyle.js
+++ js/FreeStyle.js
@@ -2,15 +2,20 @@ console.log("fuck you!")
 
 
 var btns = document.getElementsByClassName("btnStyle")
+var divs = document.getElementsByClassName("content")
 
 for (var i = 0; i < btns.length; i++) {
-    btns[i].onclick = function (e) {
 
-        for (var j = 0; j < btns.length; j++) {
-            btns[j].className = "btnStyle"
+
+    (function (n) {
+        btns[n].onclick = function(){
+            for (var j = 0; j < btns.length; j++) {
+                btns[j].className = "btnStyle"
+                divs[j].style.display = "none"
+            }
+    
+            this.className += " active"
+            divs[n].style.display = "block"
         }
-
-        this.className += " active"
-
-    }
+    }(i))
 }
