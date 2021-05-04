b95e313afe8bb11a4756e1ec14156f61b2b2540d
Micah <sep85911@outlook.com>
2021-05-04 23:32:13 +0800

    202105042332

---
+++ js/Model001.js
@@ -0,0 +1,16 @@
+function Person(id, name) {
+    this.id = id;
+    this.name = name;
+}
+
+Person.prototype.say = function(){
+    console.log("My name is " + this.name);
+}
+
+var p1 = new Person(1,"tangyao");
+var p2 = new Person(2,"Chenjing");
+
+p1.say();
+p2.say();
+
+console.log("yes")
