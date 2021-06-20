fc4eb98f51cde692a27ca31848945f4e4698f649
Micah <sep85911@outlook.com>
2021-06-20 20:50:46 +0800

    202106202050

--- js/FreeStyle.js
+++ js/FreeStyle.js
@@ -20,136 +20,17 @@ for (var i = 0; i < btns.length; i++) {
     }(i))
 }
 
-function fn(a) {
-    console.log(a)
 
-    var a = 123;
+function ShowPic(whichpic){
+    var adafa = whichpic.getAttribute("href");
+    var imgg = document.getElementById("placehold");
+    imgg.setAttribute("src",adafa)
+    console.log(adafa)
 
-    console.log(a)
+    var sText = whichpic.title;
 
-    function a() {
+    var ppp = document.getElementById("description");
 
-    }
+    ppp.firstChild.nodeValue = sText;
 
-    console.log(a)
-
-    var b = function () { }
-
-    console.log(b)
-
-    function d() { }
-}
-
-function test(a, b) {
-    console.log(a)
-    c = 0;
-    var c;
-    a = 3;
-    b = 2;
-    console.log(b)
-    function b() { }
-    function d() { }
-    console.log(b)
-}
-
-function test(a, b) {
-    console.log(a);
-    console.log(b);
-    var b = 234;
-    console.log(b)
-    a = 123;
-    console.log(a);
-    function a() { }
-    var a;
-    b = 234;
-    var b = function () { }
-    console.log(a);
-    console.log(b);
-}
-
-test(1)
-
-// AO{
-//     a:123;
-//     b:function (){};
-// }
-
-// function a(){}
-// undefined
-// 234
-// 123
-// 123
-// function (){}
-
-
-var array02 = { 1: 1, "cuker": 2 };  //映射
-var array01 = [[312, 32, 2, array02], 1, 2, 3, 4, 5]   //数组
-function func001(a) {
-    var sName = "Tangyao"
-
-    if (sName == "tangyao") {
-        console.log("Yes");
-    } else {
-        console.log("No")
-    }
-
-    while (a < 10) {
-
-        console.log("这是:", a);
-        a++
-    }
-}
-
-$(".myBtn001")[0].onclick = function () {
-
-    console.log("myBtn001 is clicking")
-    
-}
-
-
-var array03 = [31,534,674,423]
-
-$.each(array01,function (index, value) {
-    console.log(index, value);
-})
-
-
-func001(4)
-
-function funct002() {
-    console.log("clicking");
-}
-
-document.addEventListener("keyup", mykeydown)
-
-document.addEventListener("onclick", wholeClick)
-
-function mykeydown(ev) {
-    if (ev.keyCode == 83) {
-        console.log("s");
-    }
-    console.log(ev.keyCode);
-}
-
-function wholeClick(ev) {
-    console.log(ev)
-}
-
-var arr = [11,33,55,77,99];
-
-arr.forEach(function(value,index){
-    console.log(value,index);
-})
-
-
-let dd = document.getElementsByClassName("dd")[0]
-
-// console.log('dd.childNodes.length:' + dd.childNodes.length)
-
-
-function ShowPic(whichpic)
-{
-    var address = whichpic.getAttribute("href");
-    var placehold = document.getElementById("placehold");
-    placehold.setAttribute("src",address);
 }
