0eca34f6b65adc3eb46f6f00db93bd06e9552b42
Micah <sep85911@outlook.com>
2021-06-27 21:47:29 +0800

    202106262147

--- js/FreeStyle.js
+++ js/FreeStyle.js
@@ -39,3 +39,31 @@ function popUp(url)
     console.log("popUp")
     window.open(url,"baidu","width=320,height=480");
 }
+
+function ImageGallery(){
+    let images = document.getElementsByTagName("a")
+
+    for(var i = 0; i < images.length; i ++){
+        images[i].onclick = function(){
+            ShowPic(this);
+            return false;
+        }
+    }
+}
+
+function AddLoadEvnet(func){
+    var oldfunc = window.onload;
+
+    if(typeof window.onload != "function"){
+        window.onload = func
+    }else
+    {
+        window.onload = function (){
+            oldfunc();
+            func();
+        }
+    }
+}
+
+
+AddLoadEvnet(ImageGallery);
