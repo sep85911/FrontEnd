897647ce17b4dc0e1586e8095cb19c4f771cc60a
Micah <sep85911@outlook.com>
2024-04-08 11:05:03 +0800

    202404081105

---
+++ js/BaseTest.js
@@ -0,0 +1,15 @@
+
+let btn001 = document.getElementById("btn001")
+
+btn001.innerText = "测试"
+btn001.addEventListener("click",OnBtn001Pressed)
+
+function OnBtn001Pressed(params) {
+
+    let arr = [1,2,3,4,4,5,6,7,8,9,9,534,535,67453,637,56,523453,45] 
+
+    for (let index = 0; index < arr.length; index++) {
+        console.log(arr[index]);
+    }
+}
+
