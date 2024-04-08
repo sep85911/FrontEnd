40baa8132896f9e421926f4098a3e631fa5c02f7
Micah <sep85911@outlook.com>
2024-04-08 11:18:25 +0800

    202404081118

--- js/BaseTest.js
+++ js/BaseTest.js
@@ -9,7 +9,7 @@ function OnBtn001Pressed(params) {
     let arr = [1,2,3,4,4,5,6,7,8,9,9,534,535,67453,637,56,523453,45] 
 
     for (let index = 0; index < arr.length; index++) {
-        console.log(arr[index]);
+        btn001.innerText = "测试" + index
     }
 }
 
